3dfee8eaa7b922ca44bf7f286501874539f00fa9
Amit <amit@clickapps.co>
2018-02-06 14:29:05 +0530

    Changes

--- app/src/main/java/test/util/MainActivity.java
+++ app/src/main/java/test/util/MainActivity.java
@@ -1,7 +1,8 @@
-package com.util;
+package test.util;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
