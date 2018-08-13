0563a5259c611d28341e343a7bdd42eb81e2f8a3
Amit <amit@clickapps.co>
2018-08-13 12:03:23 +0530

    Commit for Animation util and color util

--- util/src/main/java/com/util/categories/AnimationUtil.java
+++ util/src/main/java/com/util/categories/AnimationUtil.java
@@ -6,6 +6,8 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import com.util.R;
+
 
 public final class AnimationUtil {
 
@@ -26,6 +28,59 @@ public final class AnimationUtil {
     public static void animate(View view, @AnimRes int anim) {
         Animation animation =
                 AnimationUtils.loadAnimation(view.getContext(), anim);
+        view.startAnimation(animation);
+    }
+
+    public static void pulse(View view) {
+        animate(view, R.anim.pulse_out_repeat, false);
+    }
+
+    public static void fadeIn(View view) {
+        animate(view, android.R.anim.fade_in, false);
+    }
+
+    public static void fadeOut(View view) {
+        animate(view, android.R.anim.fade_out, false);
+    }
+
+    public static void animateFromResource(final View view, int animationId) {
+        animate(view, animationId, false);
+    }
+
+    public static void animateInFromResource(final View view, int animationId) {
+        animate(view, animationId, false);
+    }
+
+    public static void animateOutFromResource(final View view, int animationId) {
+        animate(view, animationId, true);
+    }
+
+    private static void animate(final View view, int animationId, final boolean out) {
+        if (view == null) return;
+
+        final Animation animation = AnimationUtils.loadAnimation(view.getContext(), animationId);
+        final float alpha = view.getAlpha();
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (out) {
+                    view.setVisibility(View.GONE);
+                    view.setAlpha(alpha);
+                }
+            }
+        });
+
+        if (!out) view.setVisibility(View.VISIBLE);
+
         view.startAnimation(animation);
     }
 }
--- util/src/main/java/com/util/categories/ColorUtil.java
+++ util/src/main/java/com/util/categories/ColorUtil.java
@@ -1,6 +1,8 @@
 package com.util.categories;
 
 
+import android.graphics.Color;
+
 public final class ColorUtil {
     private ColorUtil() {
 
@@ -21,5 +23,57 @@ public final class ColorUtil {
         }
         originalColor = originalColor.replace("#", "#" + alphaHex);
         return originalColor;
+    }
+
+    public static int lighten(int color, double fraction) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        red = lightenColor(red, fraction);
+        green = lightenColor(green, fraction);
+        blue = lightenColor(blue, fraction);
+        int alpha = Color.alpha(color);
+        return Color.argb(alpha, red, green, blue);
+    }
+
+    public static int darken(int color, double fraction) {
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        red = darkenColor(red, fraction);
+        green = darkenColor(green, fraction);
+        blue = darkenColor(blue, fraction);
+        int alpha = Color.alpha(color);
+
+        return Color.argb(alpha, red, green, blue);
+    }
+
+    private static int darkenColor(int color, double fraction) {
+        return (int) Math.max(color - (color * fraction), 0);
+    }
+
+    private static int lightenColor(int color, double fraction) {
+        return (int) Math.min(color + (color * fraction), 255);
+    }
+
+    /**
+     * 颜色加深处理
+     *
+     * @param RGBValues RGB的值，由alpha（透明度）、red（红）、green（绿）、blue（蓝）构成，
+     *                  Android中我们一般使用它的16进制，
+     *                  例如："#FFAABBCC",最左边到最右每两个字母就是代表alpha（透明度）、
+     *                  red（红）、green（绿）、blue（蓝）。每种颜色值占一个字节(8位)，值域0~255
+     *                  所以下面使用移位的方法可以得到每种颜色的值，然后每种颜色值减小一下，在合成RGB颜色，颜色就会看起来深一些了
+     * @return
+     */
+    public static int colorBurn(int RGBValues) {
+        int alpha = RGBValues >> 24;
+        int red = RGBValues >> 16 & 0xFF;
+        int green = RGBValues >> 8 & 0xFF;
+        int blue = RGBValues & 0xFF;
+        red = (int) Math.floor(red * (1 - 0.1));
+        green = (int) Math.floor(green * (1 - 0.1));
+        blue = (int) Math.floor(blue * (1 - 0.1));
+        return Color.rgb(red, green, blue);
     }
 }
